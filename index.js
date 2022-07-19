07f27c0aa4f9c7b2cbdfad622c263c10188c42c7
skrowll <lucasdejesus.rocha@hotmail.com>
2022-07-19 17:06:55 -0300

    Requisito 2 OK

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const app = express();
 app.use(bodyParser.json());
 
 const HTTP_OK_STATUS = 200;
+const HTTP_NOTFOUND_STATUS = 404;
 const PORT = '3000';
 
 const talkersDbFile = 'talker.json'; 
@@ -21,6 +22,16 @@ app.get('/talker', async (_req, res) => {
     res.status(HTTP_OK_STATUS).send([]);
   }
   res.status(HTTP_OK_STATUS).send(talkers);
+});
+
+app.get('/talker/:id', async (req, res) => {
+  const { id } = req.params;
+  const talkers = await readContentFile(talkersDbFile);
+  const talker = talkers.find((element) => Number(element.id) === Number(id));
+  if (!talker) {
+    res.status(HTTP_NOTFOUND_STATUS).send({ message: 'Pessoa palestrante não encontrada' });
+  }
+  res.status(HTTP_OK_STATUS).send(talker);
 });
 
 app.all('*', (req, res) => res.status(404).json({ message: `Rota '${req.path}' não existe!` }));
